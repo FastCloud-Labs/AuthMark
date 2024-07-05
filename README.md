9d3de3bb35e68146a7a5ab05c3797415165edf29
Dan O <diydan82@gmail.com>
2024-07-05 21:46:19 +0200

    add about for more contect of problem and potential solution

--- README.md
+++ README.md
@@ -3,33 +3,43 @@
 ## dAGI Hackathon Categories
 
 #### AI-Powered Decentralized Content Authenticator
-A platform that uses AI to detect and validate the authenticity of digital content, leveraging blockchain for immutable record-keeping of content provenance and creator reputation, thus combating fake news and copyright infringement.
+
+A platform that uses AI to detect and validate the authenticity of digital content, leveraging blockchain for immutable
+record-keeping of content provenance and creator reputation, thus combating fake news and copyright infringement.
 
 ### Fake content detection and validation
 
-The cost of generating fake text, images, video, and voice is dropping, making this problem more acute than rogue AGI. Cryptographic content authentication and decentralized creator reputation can help solve this problem globally.
-
+The cost of generating fake text, images, video, and voice is dropping, making this problem more acute than rogue AGI.
+Cryptographic content authentication and decentralized creator reputation can help solve this problem globally.
 
 ## Problems
 
-Detecting Synthetic Content is hard, deepfakes and voice cloning are becoming indistinguishable from real, this are powering a new wave of sophisticated scams and crimes.
+Detecting Synthetic Content is hard, deepfakes and voice cloning are becoming indistinguishable from real, this are
+powering a new wave of sophisticated scams and crimes.
 Misinformation and fake news are becoming a global problem, and the current solutions are not enough to stop it.
 
 ## Solutions
 
 ### 1) Prevention
-Adding digital watermarks, trojan horses and "poisoning" the data used to train the AI models can help to detect synthetic content.
-### 2) Detection 
+
+Adding digital watermarks, trojan horses and "poisoning" the data used to train the AI models can help to detect
+synthetic content.
+
+### 2) Detection
+
 Detection of fake content using AI models that can detect the subtle differences between real and synthetic content.
 
 ### 3) Authenticity and Ownership and Verification & deAP (Decentralized Authenticity Protocol)
 
-
+            Using blockchain to store the provenance of the content and the reputation of the creator, this can help
+            to verify the authenticity of the content and the ownership of the content.
 
 ### Feature
+
 // todo
 
-## Use Cases 
+## Use Cases
+
 // todo
 
 
--- app/frontend/src/pages/about.vue
+++ app/frontend/src/pages/about.vue
@@ -1,7 +1,64 @@
 <template>
-  <Landing />
+
+  <v-container class="fill-height main">
+    <v-responsive
+      class="align-centerfill-height mx-auto"
+      max-width="1000"
+    >
+      <h1>AI-Powered Decentralized Content Authenticator</h1>
+      <p class="my-2"> A platform that uses AI to detect and validate the authenticity of digital content, leveraging
+        blockchain for
+        immutable record-keeping of content provenance and creator reputation, thus combating fake news and copyright
+        infringement.
+      </p>
+      <v-divider class="my-4"></v-divider>
+      <h1>Fake content detection and validation</h1>
+      <p class="my-2">
+        The cost of generating fake text, images, video, and voice is dropping, making this problem more acute than
+        rogue
+        AGI. Cryptographic content authentication and decentralized creator reputation can help solve this problem
+        globally.
+      </p>
+      <v-divider class="my-4"></v-divider>
+      <h2> Problems</h2>
+
+      Detecting Synthetic Content is hard, deepfakes and voice cloning are becoming indistinguishable from real, this
+      are powering a new wave of sophisticated scams and crimes.
+      Misinformation and fake news are becoming a global problem, and the current solutions are not enough to stop it.
+      <v-divider class="my-4"></v-divider>
+      <h2>Solutions</h2>
+      <ol class="ml-9 mt-2">
+        <li>
+          <h3>Prevention
+          </h3>
+          <p class="ma-2">Adding digital watermarks, trojan horses and "poisoning" the data used to train the AI models
+            can help to
+            detect
+            synthetic content.</p>
+        </li>
+        <li>
+          <h3>Detection
+          </h3>
+          <p class="ma-2">
+            Detection of fake content using AI models that can detect the subtle differences between real and
+            synthetic
+            content.
+          </p>
+        </li>
+        <li>
+          <h3>Authenticity and Ownership and Verification & deAP (Decentralized Authenticity Protocol)</h3>
+          <p class="ma-2">
+            Using blockchain to store the provenance of the content and the reputation of the creator, this can help
+            to verify the authenticity of the content and the ownership of the content.
+          </p>
+        </li>
+      </ol>
+      <v-divider class="my-4"></v-divider>
+    </v-responsive>
+  </v-container>
+
 </template>
 
 <script lang="ts" setup>
-  //
+//
 </script>
